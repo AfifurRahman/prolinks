dee431bbef90fb8cbe84fefe4955a498654f0d8c
Jean Vieri <jeansusilo99@gmail.com>
2024-05-08 19:51:04 +0300

    Fixed export discussion

--- resources/views/adminuser/discussion/export/export_discussion.blade.php
+++ resources/views/adminuser/discussion/export/export_discussion.blade.php
@@ -1,12 +1,12 @@
 <table style="border:solid 1px #000000;">
 	<thead>
 		<tr>
-			<th style="text-align:left; font-size:17px">
-				<h3>Project Rome : Q&A All Questions</h3>
-			</th>
+			<th style="text-align:left; font-size:17px;font-weight:bold;" colspan="12">
+				<h3>{{'Project Room : Q&A All Questions' }}</h3>
+			<th>
 		</tr>
 		<tr>
-			<th style="text-align:center; font-size:15px">
+			<th style="text-align:left; font-size:17px;font-weight:bold;" colspan="12">
 				<h3>As of: {{ now()->format('M d, Y h:i A T') }}</h3>
 			</th>
 		</tr>
@@ -17,33 +17,33 @@
 			<th>&nbsp;</th>
 		</tr>
 		<tr>
-			<th>Question ID</th>
-			<th>Question Title</th>
-			<th>Priority</th>
-			<th>Current Status</th>
-            <th>Submitted Date</th>
-			<th>Submitted By User</th>
-            <th>Last Updated On</th>
-            <th>Question</th>
-			<th>Latest Answer</th>
-			<th>Total Number of Followups</th>
-			<th>Total Number of Answers</th>
-			<th>Related Document(s)</th>
+			<th style="width:120px;">Question ID</th>
+			<th style="width:120px;">Question Title</th>
+			<th style="width:120px;">Priority</th>
+			<th style="width:120px;">Current Status</th>
+            <th style="width:120px;">Submitted Date</th>
+			<th style="width:120px;">Submitted By User</th>
+            <th style="width:120px;">Last Updated On</th>
+            <th style="width:120px;">Question</th>
+			<th style="width:120px;">Total Number of Answers</th>
+			<th style="width:120px;">Related Document(s)</th>
 		</tr>
 	</thead>
 	<tbody>
 		@foreach($report as $key => $value)
             <tr>
-				<td style="width:120px;">{{ $loop->iteration }}</td>
+				<td style="width:120px;">{{ $value->id }}</td>
 				<td style="width:120px;">{{ $value->subject }}</td>
-				<td style="width:120px;">{!! \globals::get_username($value->submitter) !!}</td>
-				<td style="width:120px;">{!! \globals::label_qna_status($value->status) !!}</td>
 				<td style="width:120px;">{!! \globals::label_qna_priority($value->priority) !!}</td>
-                <td style="width:120px;">{{ $value->created_submitter }}</td>
+				<td style="width:120px;">{!! \globals::label_qna_status($value->status) !!}</td>
+				<td style="width:120px;">{{ $value->created_at }}</td>
+				<td style="width:120px;">{!! \globals::get_username($value->submitter) !!}</td>
+				<td style="width:120px;">{{ $value->updated_at }}</td>
 				<td style="width:120px;">{{ $value->content }}</td>
-                <td style="width:120px;">{!! \globals::get_username($value->comment_by) !!}</td>
-				<td style="width:120px;">{{ $value->created_comment }}</td>
+                <td style="width:120px;">{{ DB::table('discussion_comments')->where('discussion_id', $value->discussion_id)->count() - 1 }}</td>
+				<td style="width:120px;">{{ DB::table('discussion_attach_files')->where('comment_id',$value->id)->value('file_name') }}</td>
 			</tr>
         @endforeach
 	</tbody>
+
 </table>
