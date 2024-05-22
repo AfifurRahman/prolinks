adec124c62826864e78b1c4a98c40b6725cfb627
Jean Vieri <jeansusilo99@gmail.com>
2024-05-22 16:06:36 +0300

    view update

--- routes/web.php
+++ routes/web.php
@@ -160,7 +160,7 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 		Route::get('documents/download/zip/{link}','App\Http\Controllers\Adminuser\DocumentController@DownloadZip')->name('adminuser.documents.downloadzip');
 		Route::get('documents/search/id','App\Http\Controllers\Adminuser\DocumentController@Search')->name('adminuser.documents.search');
 		Route::get('documents/zip/{folder}', 'App\Http\Controllers\Adminuser\DocumentController@DownloadFolder')->name('adminuser.documents.downloadfolder');
-		Route::get('documents/views/{file}', 'App\Http\Controllers\Adminuser\DocumentController@ViewFile')->name('adminuser.documents.view');
+		Route::get('documents/view/{file}', 'App\Http\Controllers\Adminuser\DocumentController@ViewFile')->name('adminuser.documents.view');
 		Route::post('documents/delete/folder','App\Http\Controllers\Adminuser\DocumentController@DeleteFolder')->name('adminuser.documents.deletefolder');
 		Route::post('documents/select','App\Http\Controllers\Adminuser\DocumentController@DownloadFiles')->name('adminuser.documents.downloadfiles');
 		Route::post('documents/delete/file','App\Http\Controllers\Adminuser\DocumentController@DeleteFile')->name('adminuser.documents.deletefile');
