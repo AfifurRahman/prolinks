3d9110a916e184e7b44a287bdb25398f35bbb60c
Afifur Rahman <afifurrahman27@gmail.com>
2024-05-29 10:22:13 +0700

    update

--- resources/views/adminuser/document/index.blade.php
+++ resources/views/adminuser/document/index.blade.php
@@ -13,6 +13,14 @@
 
 @section('content')
     <link href="{{ url('clientuser/documentindex.css') }}" rel="stylesheet" type="text/css" />
+    <style>
+        .custom-modal-header {
+            padding: 5px;
+            width: 95%;
+            margin: 0 auto;
+            margin-top: 13px;
+        }
+    </style>
     <!--Upload Modal-->
     <div id="upload-modal" class="modal">
         <div class="modal-content">
@@ -210,131 +218,139 @@
     </div>
 
      <!-- Permission -->
-     <div id="permission-modal" class="modal">
-        <div class="modal-content-large">
-            <div class="modal-topbar">
-                <div class="upload-modal-title">
-                    <h5 class="modal-title-text">Permission Settings</h5>
+    <div id="modal-add-permission" class="modal fade" role="dialog" aria-labelledby="myModalLabel" data-backdrop="static" keyboard="false" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content" style="width: 100%;">
+                <div class="modal-header">
+                    <div class="custom-modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                        <h4 class="modal-title" id="titleModal">
+                            Permission Settings
+                        </h4>
+                    </div>
                 </div>
-                <button class="modal-close" onclick="document.getElementById('permission-modal').style.display='none'">
-                    <image class="modal-close-ico" src="{{ url('template/images/icon_menu/close.png') }}"></image>
-                </button>
-            </div>
-
-            <div class="modal-permission-body">
-                <div class="permission-user-list">
-                    <h4>Users</h4>
-                    <table id="permission-user-list-table" >
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @if(count($listusers) > 0) 
-                                @foreach($listusers->unique('group_id') as $group)
-                                    @if(!empty(DB::table('access_group')->where('group_id', $group->group_id)->value('group_name')))
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="permission-user-listx">
+                                <h4>Users</h4>
+                                <table id="permission-user-list-table" >
+                                    <thead>
                                         <tr>
-                                            <td>
-                                                <image class="fol-fil-icon" src="{{ url('template/images/icon_menu/group.png') }}" />
-                                            </td>
-                                            <td class="permission-user-list-td">
-                                                {{ DB::table('access_group')->where('group_id', $group->group_id)->value('group_name') }}
-                                            </td>
+                                            <th></th>
+                                            <th></th>
                                         </tr>
-                                        @foreach($listusers as $user)
-                                            @if(!is_null(DB::table('assign_project')->where('project_id', explode('/', $origin)[2])->where('user_id', $user->user_id)->value('email')))
-                                                @if($user->group_id == $group->group_id)
-                                                    <tr>
-                                                        <td></td>
-                                                        <td>
-                                                            <div style="cursor:pointer;">
-                                                                <a onclick="checkUserPermission('{{ $user->user_id }}')">
-                                                                    <p class="permission-user-list-td">{{ $user->name }}</p>
-                                                                    <p class="permission-user-list-td2">
-                                                                    {{ $user->role == 0 ? 'Administrator' : ($user->role == 1 ? 'Collaborator' : 'Client') }}
-                                                                    </p>
-                                                                </a>
-                                                            </div>
-                                                        </td>
-                                                    </tr>
-                                                @endif
-                                            @endif
-                                        @endforeach
-                                    @endif
-                                @endforeach
-                                @foreach($listusers->unique('group_id') as $group)
-                                    @if(empty(DB::table('access_group')->where('group_id', $group->group_id)->value('group_name')))
-                                        @foreach($listusers as $user)
-                                            @if(!is_null(DB::table('assign_project')->where('project_id', explode('/', $origin)[2])->where('user_id', $user->user_id)->value('email')))
-                                                @if(empty($user->group_id))
+                                    </thead>
+                                    <tbody>
+                                        @if(count($listusers) > 0) 
+                                            @foreach($listusers->unique('group_id') as $group)
+                                                @if(!empty(DB::table('access_group')->where('group_id', $group->group_id)->value('group_name')))
                                                     <tr>
                                                         <td>
-                                                            <image class="fol-fil-icon" src="{{ url('template/images/icon_menu/user.png') }}" />
-                                                            <br>
-                                                            &nbsp;
+                                                            <image class="fol-fil-icon" src="{{ url('template/images/icon_menu/group.png') }}" />
                                                         </td>
-                                                        <td style="cursor:pointer;">
-                                                            <div style="cursor:pointer;">
-                                                                <a onclick="checkUserPermission('{{ $user->user_id }}')">
-                                                                    <p class="permission-user-list-td">{{ $user->name }}</p>
-                                                                    <p class="permission-user-list-td2">
-                                                                    {{ $user->role == 0 ? 'Administrator' : ($user->role == 1 ? 'Collaborator' : 'Client') }}
-                                                                    </p>
-                                                                </a>
-                                                            </div>
+                                                        <td class="permission-user-list-td">
+                                                            {{ DB::table('access_group')->where('group_id', $group->group_id)->value('group_name') }}
                                                         </td>
                                                     </tr>
+                                                    @foreach($listusers as $user)
+                                                        @if(!is_null(DB::table('assign_project')->where('project_id', explode('/', $origin)[2])->where('user_id', $user->user_id)->value('email')))
+                                                            @if($user->group_id == $group->group_id)
+                                                                <tr>
+                                                                    <td></td>
+                                                                    <td>
+                                                                        <div style="cursor:pointer;">
+                                                                            <a onclick="checkUserPermission('{{ $user->user_id }}')">
+                                                                                <p class="permission-user-list-td">{{ $user->name }}</p>
+                                                                                <p class="permission-user-list-td2">
+                                                                                {{ $user->role == 0 ? 'Administrator' : ($user->role == 1 ? 'Collaborator' : 'Client') }}
+                                                                                </p>
+                                                                            </a>
+                                                                        </div>
+                                                                    </td>
+                                                                </tr>
+                                                            @endif
+                                                        @endif
+                                                    @endforeach
                                                 @endif
+                                            @endforeach
+                                            @foreach($listusers->unique('group_id') as $group)
+                                                @if(empty(DB::table('access_group')->where('group_id', $group->group_id)->value('group_name')))
+                                                    @foreach($listusers as $user)
+                                                        @if(!is_null(DB::table('assign_project')->where('project_id', explode('/', $origin)[2])->where('user_id', $user->user_id)->value('email')))
+                                                            @if(empty($user->group_id))
+                                                                <tr>
+                                                                    <td>
+                                                                        <image class="fol-fil-icon" src="{{ url('template/images/icon_menu/user.png') }}" />
+                                                                        <br>
+                                                                        &nbsp;
+                                                                    </td>
+                                                                    <td style="cursor:pointer;">
+                                                                        <div style="cursor:pointer;">
+                                                                            <a onclick="checkUserPermission('{{ $user->user_id }}')">
+                                                                                <p class="permission-user-list-td">{{ $user->name }}</p>
+                                                                                <p class="permission-user-list-td2">
+                                                                                {{ $user->role == 0 ? 'Administrator' : ($user->role == 1 ? 'Collaborator' : 'Client') }}
+                                                                                </p>
+                                                                            </a>
+                                                                        </div>
+                                                                    </td>
+                                                                </tr>
+                                                            @endif
+                                                        @endif
+                                                    @endforeach
+                                                @endif
+                                            @endforeach
+                                        @endif
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="permission-file-listx">
+                                <h4 id="permissionUser">Select a user</h4>
+                                <table id="permission-file-list-table" width="100%" class="table table-hover table-sm">
+                                    <thead>
+                                        <tr>
+                                            <th>
+                                                File Name
+                                            </th>
+                                            <th>
+                                                <input type="checkbox" id="all_checkbox" class="setPermissionBox" style="width:30px; height:16px;"></input>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="fileList">
+                                        @foreach($fileList as $file)
+                                            @if( DB::table('upload_files')->where('basename', $file)->value('status') == 1 )
+                                                <tr>
+                                                    <td>
+                                                        {{DB::table('upload_files')->where('basename', $file)->value('name')}}
+                                                    </td>
+                                                    <td>
+                                                        <input type="checkbox" id="{{$file}}" class="setPermissionBox" value="{{$file}}" style="width:30px; height:16px;"></input>
+                                                    </td>
+                                                </tr>
                                             @endif
                                         @endforeach
-                                    @endif
-                                @endforeach
-                            @endif
-                        </tbody>
-                    </table>
-                </div> 
-                <div class="permission-file-list">
-                    <h4 id="permissionUser">Select a user</h4>
-                    <table id="permission-file-list-table" class="table table-sm">
-                        <thead>
-                            <tr>
-                                <th>
-                                    File Name
-                                </th>
-                                <th>
-
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody id="fileList">
-                            @foreach($fileList as $file)
-                                @if( DB::table('upload_files')->where('basename', $file)->value('status') == 1 )
-                                    <tr>
-                                        <td>
-                                            {{DB::table('upload_files')->where('basename', $file)->value('name')}}
-                                        </td>
-                                        <td>
-                                            <input type="checkbox" id="{{$file}}" class="setPermissionBox" value="{{$file}}"></input>
-                                        </td>
-                                    </tr>
-                                @endif
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div> 
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="pull-right">
+                                <a class="cancel-btn" data-dismiss="modal">Cancel</a>
+                                <button class="create-btn" id="setPermissionButton" onclick="savePermission()">Save settings</button>
+                            </div>
+                        </div>
+                    </div>
+                    <input type="hidden" id="IDuser" value="">
+                </div>
             </div>
-
-            <input type="hidden" id="IDuser" value="">
-
-            <div class="modal-footer">
-                <a class="cancel-btn" onclick="document.getElementById('permission-modal').style.display='none'">Cancel</a>
-                <button class="create-btn" id="setPermissionButton" onclick="savePermission()">Save settings</button>
-            </div>  
         </div>
     </div>
-
 
     <div class="box_helper">
         <h2 id="title" style="color:black;font-size:28px;">
@@ -350,7 +366,8 @@
             @endif
             
             @if(Auth::user()->type == \globals::set_role_administrator())
-                <button class="permissions" onclick="setPermission()">Permissions</button>
+                <button data-target="#modal-add-permission" onclick="setPermission()" data-toggle="modal" class="permissions">Permission</button>
+                <!-- <button class="permissions" onclick="setPermission()">Permissions</button> -->
             @endif
 
             @if(Auth::user()->type == \globals::set_role_collaborator() OR Auth::user()->type == \globals::set_role_administrator())
@@ -992,7 +1009,7 @@
         }
 
         function setPermission() {
-            document.getElementById('permission-modal').style.display='block';
+            // document.getElementById('permission-modal').style.display='block';
             document.getElementById('permission-file-list-table').style.display="none";
             $('#setPermissionButton').prop("disabled", true);
 
@@ -1029,7 +1046,8 @@
         }
 
         function savePermission() {
-            document.getElementById('permission-modal').style.display='none';
+            // document.getElementById('permission-modal').style.display='none';
+            $("#modal-add-permission").modal('hide');
             showNotification('Permission settings saved');
         }
 
@@ -1223,6 +1241,13 @@
                 });
             });
         }
+
+        $("#permission-file-list-table").dataTable({
+            "bPaginate": false,
+            "bInfo": false,
+            "bSort": false,
+            "bAutoWidth": false,
+        });
     </script>
     @endpush
 @endsection
