663aa3a701a18a26ba425234295e0d6794329c4c
Afifur Rahman <afifurrahman27@gmail.com>
2024-06-10 12:32:28 +0700

    update

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,8 +6,8 @@
 @section('content')
     <div class="card-box">
         <center>
-            <h1>Access Denied</h1>
-            <img src="{{ url('template/images/access-denied.png') }}" /><br><br>
+            <h1>Not Found</h1>
+            <img src="{{ url('template/images/not-found.png') }}" width="200" height="200" /><br><br>
             <p>It is a long established fact that a reader will be distracted by the readable content of a<br> page when looking at its layout.</p>
         </center>    
     </div>
