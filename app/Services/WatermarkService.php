d4be1a0ddbdd60168465bc2997c3817ca64fbbac
Jean Vieri <jeansusilo99@gmail.com>
2024-06-28 17:11:08 +0700

    watermark update

--- app/Services/WatermarkService.php
+++ app/Services/WatermarkService.php
@@ -13,6 +13,55 @@ use Auth;
 class WatermarkService extends Fpdi
 {
     protected $extgstates = array();
+    var $angle = 0;
+
+    function Rotate($angle,$x=-1,$y=-1)
+    {
+        if($x==-1)
+            $x=$this->x;
+        if($y==-1)
+            $y=$this->y;
+        if($this->angle!=0)
+            $this->_out('Q');
+        $this->angle=$angle;
+        if($angle!=0)
+        {
+            $angle*=M_PI/180;
+            $c=cos($angle);
+            $s=sin($angle);
+            $cx=$x*$this->k;
+            $cy=($this->h-$y)*$this->k;
+            $this->_out(sprintf('q %.5F %.5F %.5F %.5F %.2F %.2F cm 1 0 0 1 %.2F %.2F cm',$c,$s,-$s,$c,$cx,$cy,-$cx,-$cy));
+        }
+    }
+
+    function _endpage()
+    {
+        if ($this->angle != 0) {
+            $this->angle = 0;
+            $this->_out('Q');
+        }
+        parent::_endpage();
+    }
+
+    function RotatedText($x,$y,$txt,$angle)
+    {
+        //Text rotated around its origin
+        $this->Rotate($angle,$x,$y);
+        $this->Text($x,$y,$txt);
+        $this->Rotate(0);
+    }
+
+
+    function MultiLineRotatedText($x, $y, $txt, $angle, $lineHeight)
+    {
+        $lines = explode("\n", $txt);
+        foreach ($lines as $i => $line) {
+            $lineWidth = $this->GetStringWidth($line);
+            $centeredX = $x - ($lineWidth / 2);
+            $this->RotatedText($centeredX, $y + ($i * $lineHeight) - ($y / 4), $line, $angle);
+        }
+    }
 
     protected function AddExtGState($parms)
     {
@@ -28,8 +77,7 @@ class WatermarkService extends Fpdi
 
     protected function _enddoc()
     {
-        if (!empty($this->extgstates) && $this->PDFVersion<'1.4')
-            $this->PDFVersion='1.4';
+        if (!empty($this->extgstates) && $this->PDFVersion < '1.4') $this->PDFVersion = '1.4';
         parent::_enddoc();
     }
 
@@ -42,7 +90,7 @@ class WatermarkService extends Fpdi
             $parms = $this->extgstates[$i]['parms'];
             $this->_put(sprintf('/ca %.3F', $parms['ca']));
             $this->_put(sprintf('/CA %.3F', $parms['CA']));
-            $this->_put('/BM '.$parms['BM']);
+            $this->_put('/BM ' . $parms['BM']);
             $this->_put('>>');
             $this->_put('endobj');
         }
@@ -53,7 +101,7 @@ class WatermarkService extends Fpdi
         parent::_putresourcedict();
         $this->_put('/ExtGState <<');
         foreach ($this->extgstates as $k => $extgstate)
-            $this->_put('/GS'.$k.' '.$extgstate['n'].' 0 R');
+            $this->_put('/GS' . $k . ' ' . $extgstate['n'] . ' 0 R');
         $this->_put('>>');
     }
 
@@ -65,38 +113,37 @@ class WatermarkService extends Fpdi
 
     public function addPDFWatermark($fileID)
     {
-        $pdf = new FPDI();
+        $pdf = new WatermarkService();
 
         $filePath = Storage::path(UploadFile::where('basename', $fileID)->value('directory') . '/' . $fileID);
-        $outputPath = Storage::path('temp/'. Auth::user()->user_id . '/temp');
-        $projectName = Project::where('project_id', UploadFile::where('basename', $fileID)->value('project_id'))->value('project_name'); 
+        $outputPath = Storage::path('temp/' . Auth::user()->user_id . '/temp');
+        $projectName = Project::where('project_id', UploadFile::where('basename', $fileID)->value('project_id'))->value('project_name');
         $userName = Auth::user()->name;
         $companyName = Client::where('client_id', Auth::user()->client_id)->value('client_name');
         $timestamp = date('F j, Y H:i', strtotime('now'));
 
         $watermarkText = <<<EOT
-                        Project $projectName
-                        $userName
-                        $companyName
-                        $timestamp WIB
-                        EOT;
+        Project $projectName 
+        $userName 
+        $companyName 
+        $timestamp WIB
+        EOT;
 
         $pageCount = $pdf->setSourceFile($filePath);
 
         for ($pageNo = 1; $pageNo <= $pageCount; $pageNo++) {
             $templateId = $pdf->importPage($pageNo);
             $size = $pdf->getTemplateSize($templateId);
-    
+
             $pdf->AddPage($size['orientation'], [$size['width'], $size['height']]);
             $pdf->useTemplate($templateId);
-    
+
             $pdf->SetFont('Helvetica', 'B', 24);
             $pdf->SetAlpha(0.4);
-            $pdf->SetTextColor(255,0,0); 
-            $pdf->SetXY(10, $size['height']/2 - $size['height']/7);
-    
-            $pdf->MultiCell(0, 10, $watermarkText, 0, 'C', 0, 1, '', '', true);
-    
+            $pdf->SetTextColor(255, 0, 0);
+            $pdf->SetXY(10, $size['height'] / 2 - $size['height'] / 7);
+
+            $pdf->MultiLineRotatedText($size['width'] /2 , $size['height'] / 2, $watermarkText, 0, 10);
             $pdf->SetXY(0, 0);
         }
 
@@ -108,41 +155,58 @@ class WatermarkService extends Fpdi
         $pdf->Output($outputPath, 'F');
     }
 
-    public function addIMGWatermark($fileID) {
+    public function addIMGWatermark($fileID)
+    {
         $filePath = Storage::path(UploadFile::where('basename', $fileID)->value('directory') . '/' . $fileID);
-        $outputPath = Storage::path('temp/'. Auth::user()->user_id . '/temp');
+        $outputPath = Storage::path('temp/' . Auth::user()->user_id . '/temp');
         $fileMimeType = UploadFile::where('basename', $fileID)->value('mime_type');
-        
+
         if (str_starts_with($fileMimeType, 'image/jpeg')) {
             $image = imagecreatefromjpeg($filePath);
         }
 
-        $projectName = Project::where('project_id', UploadFile::where('basename', $fileID)->value('project_id'))->value('project_name'); 
+        $projectName = Project::where('project_id', UploadFile::where('basename', $fileID)->value('project_id'))->value('project_name');
         $userName = Auth::user()->name;
         $companyName = Client::where('client_id', Auth::user()->client_id)->value('client_name');
         $timestamp = date('F j, Y H:i', strtotime('now'));
 
         $watermarkText = <<<EOT
-                        Project $projectName
-                        $userName
-                        $companyName
-                        $timestamp WIB
-                        EOT;
+        Project $projectName
+        $userName
+        $companyName
+        $timestamp WIB
+        EOT;
 
-        $fontSize = 50;
-        $angle = 36;
+        
+        $fontSizeX = imagesx($image) / 20;
+        $fontSizeY = imagesy($image) / 14;
+
+        if ($fontSizeX < $fontSizeY) {
+            $fontSize = $fontSizeX;
+        } else {
+            $fontSize = $fontSizeY;
+        }
+        $angle = 0;
         $textColor = imagecolorallocatealpha($image, 255, 0, 0, 50);
         $fontPath = storage_path('app/fonts/arial.ttf');
         $imageWidth = imagesx($image);
         $imageHeight = imagesy($image);
+
         $textBox = imagettfbbox($fontSize, $angle, $fontPath, $watermarkText);
-        $textWidth = $textBox[2] - $textBox[0];
         $textHeight = $textBox[7] - $textBox[1];
 
-        $x = ($imageWidth - $textWidth) / 2.5;
-        $y = ($imageHeight / 1.5);
 
-        imagettftext($image, $fontSize, $angle, $x, $y, $textColor, $fontPath, $watermarkText);
+        $lines = explode("\n", $watermarkText);
+
+        foreach ($lines as $i => $line) {
+            $textSize = imagettfbbox($fontSize, $angle, $fontPath, $line);
+            $textWidth = $textSize[2] - $textSize[0]; 
+
+            $centeredX = ($imageWidth/2) - ($textWidth / 2);
+            $centeredY = ($imageHeight/2) + (($fontSize + ($fontSize /2)) * ($i - 1));
+
+            imagettftext($image, $fontSize, $angle, $centeredX, $centeredY, $textColor, $fontPath, $line);
+        }
 
         imagejpeg($image, $outputPath);
     }
