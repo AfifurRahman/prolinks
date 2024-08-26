1788452973fe7af8db2455054c3cbd2465d96028
Jean Vieri <jeansusilo99@gmail.com>
2024-08-26 14:49:43 +0700

    Major Update

--- resources/views/layouts/navigation_client.blade.php
+++ resources/views/layouts/navigation_client.blade.php
@@ -10,12 +10,14 @@
                                 @if(count(\globals::get_project_sidebar()) > 0)
                                     @foreach(\globals::get_project_sidebar() as $mainProject)
                                         @if(!empty($mainProject->RefAssignProject) && count($mainProject->RefAssignProject) > 0)
-                                            <optgroup label="{{ $mainProject->project_name }}">
-                                                @if(count($mainProject->RefAssignProject) > 0)
-                                                    @foreach($mainProject->RefAssignProject as $subsProj)
-                                                        <option value="{{ $subsProj->subproject_id }}" {{ !empty(Auth::user()->session_project) && Auth::user()->session_project == $subsProj->subproject_id ? "selected":"" }} >{{ $subsProj->RefSubProject->subproject_name }}</option>
-                                                    @endforeach
-                                                @endif
+                                            <optgroup label="{{ DB::table('clients')->where('client_id', $mainProject->client_id)->value('client_name') }}">
+                                                <optgroup label="&nbsp;&nbsp;&nbsp;&nbsp;{{ $mainProject->project_name }}">
+                                                    @if(count($mainProject->RefAssignProject) > 0)
+                                                        @foreach($mainProject->RefAssignProject as $subsProj)
+                                                            <option value="{{ $subsProj->subproject_id }}" {{ !empty(Auth::user()->session_project) && Auth::user()->session_project == $subsProj->subproject_id ? "selected":"" }} >&nbsp;&nbsp;{{ $subsProj->RefSubProject->subproject_name }}</option>
+                                                        @endforeach
+                                                    @endif
+                                                </optgroup>
                                             </optgroup>
                                         @endif
                                     @endforeach
